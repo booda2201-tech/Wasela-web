d3f6f35000b43f9be3dd03b16278b55869c1d2f4
booda2201-tech <booda2201@gmail.com>
2026-04-30 19:40:01 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroComponent } from './components/home/hero/hero.component';
+import { FeaturesComponent } from './components/home/features/features.component';
+import { MerchantsComponent } from './components/home/merchants/merchants.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    HeroComponent,
+    FeaturesComponent,
+    MerchantsComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        'waseela-blue': '#1D1DDB',
+        'waseela-orange': '#FF6B00',
+      }
+    },
+  },
+  plugins: [],
+}
