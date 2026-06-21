83e4be659b69e94abb4756ffc4e5b8368d953c72
booda2201-tech <booda2201@gmail.com>
2026-06-21 15:25:18 +0300

    feat: Add Netlify redirects configuration and update environment API URLs for production and development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,18 @@
+// export const environment = {
+//   production: true,
+//   /** Production API base URL. */
+//   apiBaseUrl: 'https://waseela.somee.com/api',
+//   /** Production API origin for `/media/...` assets. */
+//   apiOrigin: 'https://waseela.somee.com',
+//   /** روابط المتاجر — املأها عند الجاهزية */
+//   appStoreUrl: '',
+//   googlePlayUrl: '',
+// };
+
 export const environment = {
   production: true,
-  /** Production API base URL. */
-  apiBaseUrl: 'https://waseela.somee.com/api',
-  /** Production API origin for `/media/...` assets. */
-  apiOrigin: 'https://waseela.somee.com',
-  /** روابط المتاجر — املأها عند الجاهزية */
+  apiBaseUrl: '/api',
+  apiOrigin: 'https://waseela.somee.com', // رجّع اللينك هنا للصور
   appStoreUrl: '',
-  googlePlayUrl: '',
+  googlePlayUrl: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,21 @@
+// export const environment = {
+//   production: false,
+//   /** Production API (local backend on :7041 is optional). */
+//   apiBaseUrl: 'https://waseela.somee.com/api',
+//   apiOrigin: 'https://waseela.somee.com',
+//   /** روابط المتاجر — املأها عند الجاهزية */
+//   appStoreUrl: '',
+//   googlePlayUrl: '',
+// };
+
+
+
+
+
 export const environment = {
   production: false,
-  /** Production API (local backend on :7041 is optional). */
-  apiBaseUrl: 'https://waseela.somee.com/api',
-  apiOrigin: 'https://waseela.somee.com',
-  /** روابط المتاجر — املأها عند الجاهزية */
+  apiBaseUrl: '/api',
+  apiOrigin: 'https://waseela.somee.com', // رجّع اللينك هنا للصور
   appStoreUrl: '',
-  googlePlayUrl: '',
+  googlePlayUrl: ''
 };
